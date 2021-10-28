77f9ac8973ab9807d896256db0b051fa6f5958dd
Daniel Silva <bukaro.dany@gmail.com>
2021-10-27 21:53:53 -0500

    Fill out dataframe with rows

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
 import requests
 import pandas as pd
 import hashlib
+import time
+
 
 def get_all_contries():
+    start = time.time()
     url = 'https://restcountries.com/v3.1/all'
     response = requests.get(url).json()
+    end = time.time()
+    execution_time = (end - start) * 1000
     
-    # country = response[0]['name']['common']
-    # # print(country)
-
-    # region = response[0]['region']
-
-    # langugages = response[0]['languages']
-    
-    # print(response[0]['region'])
-    # print(response[0]['languages'])
-
-    list_countries = []
+    #list_countries = []
+    df = pd.DataFrame(columns=['Region', 'City Name', 'Languages', 'Time'])
 
     for i in range(len(response)):
-        #print(i)
+        start = time.time_ns()
         try:
             country = response[i]['name']['common']
         except KeyError:
@@ -34,28 +30,39 @@ def get_all_contries():
             langugages = response[i]['languages']
             langugages = list(langugages.values())
             languages_enc_hex = encript_language(langugages)
-            langugages_str = '|'.join(languages_enc_hex)
+            #langugages_str = '|'.join(languages_enc_hex)
         except KeyError:
             langugages = 'NA'
-        
-        item = [region, country, langugages_str]
-        df = add_to_dataframe(item)
-        list_countries.append(item)
 
-    #df = add_to_dataframe(list_countries)
-    
+        item = [region, country, languages_enc_hex]
+        df = add_to_dataframe(df=df, data=item, start_time=start, time_req= execution_time)
+
     print(df)
 
 
-def add_to_dataframe(list_of_countries):
-    #df = pd.DataFrame(list_of_countries, columns=['Region', 'City Name', 'Languages'])
-    df = pd.DataFrame(columns=['Region', 'City Name', 'Languages'])
+def add_to_dataframe(df, data, start_time, time_req):
+    #import pdb; pdb.set_trace()
+    if len(data[2]) > 1:
+        for language in data[2]:
+            region = data[0]
+            country = data[1]
+            end = time.time_ns()
+            import pdb; pdb.set_trace()
+            row_time = ((end - start_time) * 1000) + time_req
+            data = [region, country, language, row_time]
+            temporary_df = pd.DataFrame(
+                [data], columns=['Region', 'City Name', 'Languages', 'Time'])
+            df = df.append(temporary_df, ignore_index=True)
 
-    #print(list_of_countries)
+    else:
+        end = time.time()
+        row_time = ((end - start_time) * 1000) + time_req
+        import pdb; pdb.set_trace()
+        data.append(row_time)
+        temporary_df = pd.DataFrame(
+            [data], columns=['Region', 'City Name', 'Languages', 'Time'])
+        df = df.append(temporary_df, ignore_index=True)
 
-    df = df.append(list_of_countries)
-
-    #print(df)
     return df
 
 
@@ -64,11 +71,9 @@ def encript_language(languages):
     for language in languages:
         lang_enc = hashlib.sha1(bytes(language, 'utf-8'))
         lang_hex = lang_enc.hexdigest()
-        langs_hex.append(lang_hex)        
-    
+        langs_hex.append(lang_hex)
+
     return langs_hex
-
-
 
 
 if __name__ == '__main__':
