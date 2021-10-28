238546cdb318edbb416f3355336aea97fb31378a
Daniel Silva <bukaro.dany@gmail.com>
2021-10-27 19:36:17 -0500

    Adding functino to encrypt languages

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import hashlib
 
 def get_all_contries():
     url = 'https://restcountries.com/v3.1/all'
@@ -32,23 +33,42 @@ def get_all_contries():
         try:
             langugages = response[i]['languages']
             langugages = list(langugages.values())
-            langugages_str = '|'.join(langugages)
+            languages_enc_hex = encript_language(langugages)
+            langugages_str = '|'.join(languages_enc_hex)
         except KeyError:
             langugages = 'NA'
         
         item = [region, country, langugages_str]
-
+        df = add_to_dataframe(item)
         list_countries.append(item)
 
-    df = add_to_dataframe(list_countries)
+    #df = add_to_dataframe(list_countries)
     
     print(df)
 
 
 def add_to_dataframe(list_of_countries):
-    df = pd.DataFrame(list_of_countries, columns=['Region', 'City Name', 'Languages'])
+    #df = pd.DataFrame(list_of_countries, columns=['Region', 'City Name', 'Languages'])
+    df = pd.DataFrame(columns=['Region', 'City Name', 'Languages'])
 
+    #print(list_of_countries)
+
+    df = df.append(list_of_countries)
+
+    #print(df)
     return df
+
+
+def encript_language(languages):
+    langs_hex = []
+    for language in languages:
+        lang_enc = hashlib.sha1(bytes(language, 'utf-8'))
+        lang_hex = lang_enc.hexdigest()
+        langs_hex.append(lang_hex)        
+    
+    return langs_hex
+
+
 
 
 if __name__ == '__main__':
